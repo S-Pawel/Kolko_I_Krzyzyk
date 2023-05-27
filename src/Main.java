090e3dd4e2b543e1c41d17c95826bde86d38476b
Pawel Szkudzinski <pawel.szkudzinski@gmail.com>
2023-05-27 13:41:47 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Hello world!");
+
+        char wybor = (char) scanner.nextInt();
+
+        if(wybor == 1){
+
+        } else if (wybor == 2) {
+
+        } else if (wybor == 3) {
+
+        }
+
+
+    }
+}
