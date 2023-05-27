e73492a5a7b95e6dd82a0804636e16fb16c3973b
Pawel Szkudzinski <pawel.szkudzinski@gmail.com>
2023-05-27 13:57:34 +0200

    More information in menu

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,23 @@ public class Main {
 
         Scanner scanner= new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        System.out.println("Witaj w grze w kółko i krzyżyk");
+        System.out.println("1 - Zagraj na dwóch graczy");
+        System.out.println("2 - Wyświetl zasady gry");
+        System.out.println("3 - Wyjście");
+
 
         char wybor = (char) scanner.nextInt();
 
         if(wybor == 1){
-
+            System.out.println("Zagraj na dwóch graczy");
         } else if (wybor == 2) {
-
+            System.out.println("Wyświetl zasady gry");
         } else if (wybor == 3) {
-
+            System.out.println("Wyjście");
+        }else {
+            System.out.println("Nieznane hasło, wpisz poprawną liczbę");
         }
-
 
     }
 }
